6994dfce58cbd8f86ffe75f69e1fe64e5ed25d5f
Akachukwu <udechukwuakachukwu@gmail.com>
2024-07-24 21:31:39 +0100

    Small correction on Hero section

--- src/pages/public/LandingPage.tsx
+++ src/pages/public/LandingPage.tsx
@@ -29,7 +29,6 @@ import { MatchCard } from "../../components/fixtures/MatchCard";
 import PublicFooter from "../../components/layout/PublicFooter";
 import { selectShowAdPopUp, setShowAdPopUp } from "../../state/slices/auth";
 import AdPopUp from "../../components/modals/AdPopUp";
-import Hero from "../../components/Hero";
 
 const LandingPage = () => {
 	const dispatch = useAppDispatch();
@@ -87,7 +86,30 @@ const LandingPage = () => {
 		<main className="bg-white">
 			<PublicHeader />
 			{/* Hero Image */}
-			<Hero/>
+			<section className="px-6 md:px-40 pt-10 lg:pt-0 max-h-screen w-full lg:flex justify-between">
+				<div className="lg:w-1/2 flex-shrink-0 lg:pt-24">
+					<h1 className="text-[50px] lg:text-[56px] font-semibold leading-[55px] lg:leading-[68px]">
+						<span className="text-[#eb1536]">Predict</span>! <br />
+						Take Your <br/> Share<span className="text-[#eb1536]">.</span>
+					</h1>
+					<p className="mt-4 mb-8 text-[#5F6B7A]">
+						Luck or skill? Let’s see who’s better.
+					</p>
+					<div className="flex items-center gap-x-5">
+						<Link to="/dashboard/fixtures">
+							<Button title="Start predicting" />
+						</Link>
+						<Link to="/dashboard/private-league">
+							<Button.OutlineRed title="Create a league" />
+						</Link>
+					</div>
+				</div>
+				<img
+					src={heroImg}
+					alt="We play ball"
+					className="hidden lg:block max-h-[90vh] -translate-x-20 -translate-y-10 transform z-0"
+				/>
+			</section>
 
 			{/* Carousel section */}
 			<div className="px-4 md:px-40 mt-10 md:-mt-96 mb-24 font-medium">
