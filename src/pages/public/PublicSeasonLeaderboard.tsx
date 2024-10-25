eb951919e387967019a367848c20fdb6cc3c733d
Emmanuel Okon <emmaudo27@gmail.com>
2024-10-25 21:45:43 +0100

    fixed type errors

--- src/pages/public/PublicSeasonLeaderboard.tsx
+++ src/pages/public/PublicSeasonLeaderboard.tsx
@@ -184,11 +184,11 @@ const PublicSeasonLeaderboard = () => {
       <main className="px-4 md:px-40 mb-20">
         <section className="w-full p-4 md:p-8">
           <Table
-            data={leaderboard?.data ?? []}
+            data={leaderboard?.result?.data ?? []}
             columns={columns}
             rows={10}
             loading={isFetchingSeasons || isFetchingSeasonLeaderboard}
-            totalPages={leaderboard?.totalPages ?? 1}
+            totalPages={leaderboard?.result?.totalPages ?? 1}
             isLeaderboardTable
             current_page={Number(page ?? 1)}
             setCurrentPage={(page: number): void => {
